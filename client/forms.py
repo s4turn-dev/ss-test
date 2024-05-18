3edbff0684653d7e276ebfe55064fa8063e38a1c
S4TURN_ <mixanidze3521@gmail.com>
2024-05-18 10:13:50 +0300

    develop: implemented web essentials

---
+++ client/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+
+from . import db
+from models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+class SignupForm(FlaskForm):
+    username = StringField('Login', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(message="Password can't be empty")])
+    confirm_password = passwordfield('Confirm password', validators=[EqualTo('password', message='Passwords do not match')])
+    submit = submitfield('Sign Up')
+
+    def validate_username(self, username):
+        if User.query.filter_by(login=username.data).first():
+            raise ValidationError('This username is already in use')
--- client/routes.py
+++ client/routes.py
@@ -7,14 +7,84 @@ from flask import request
 from flask_login import current_user
 from flask_login import login_required
 from flask_login import login_user
-from flask_login import logout_user
-from hashlib import sha256
+from hashlib import md5
 
 from . import app, login_manager, db
-#from app.forms import LoginForm, SignupForm
-#from app.models import User, MasterClass
+from app.forms import LoginForm, SignupForm
+from app.models import User#, Requisites
 
 
+# --- General stuff --- 
 @app.get('/')
-def index():
-    return 'Hello, World!'
+@login_required
+def requisitesPage():
+    return render_template('profile.html', user=current_user)
+
+@app.post('/')
+@login_required
+def newRequisite():
+    print('add req')
+    redirect(url_for('requisitesPage'))
+
+@app.post('/edit/<req_id>')
+@login_required
+def editrequisite(req_id: int):
+    print(f'edit req {req_id}')
+    redirect(url_for('requisitesPage'))
+
+@app.post('/delete/<req_id>')
+@login_required
+def editrequisite(req_id: int):
+    print(f'delete req {req_id}')
+    redirect(url_for('requisitesPage'))
+
+
+# --- Account management ---
+@login_manager.user_loader
+def user_loader(uid: int):
+    return User.query.get(uid)
+
+@login_manager.unauthorized_handler
+def unathorized():
+    return redirect(url_for('loginPage'))
+
+def hash_pwd(pwd: str) -> str: return md5(bytes(pwd, 'utf-8')).hexdigest().decode('utf-8')
+
+
+@app.get('/login')
+def loginPage():
+    if current_user.is_authenticated:
+        return redirect(url_for('requisitesPage'))
+    return render_template('login.html')
+
+@app.post('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(login=form.username.data, password=hash_pwd(form.password.data)).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('requisitesPage'))
+        else:
+            flash('Wrong credentials')
+            rturn redirect(url_for('loginPage'))
+
+
+@app.get('/signup')
+def signupPage():
+    if current_user.is_authenticated:
+        return redirect(url_for('requisitesPage'))
+    return render_template('signup.html')
+
+@app.post('/signup'):
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = hash_pwd(form.password.data) 
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for('requisitesPage'))
+    return redirect(url_for('signupPage'))
