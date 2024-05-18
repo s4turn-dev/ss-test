ba4b35b39e93b5164b36652ecb3b455467a79f5a
S4TURN_ <mixanidze3521@gmail.com>
2024-05-19 02:28:38 +0300

    develop: api/ removal additions

--- config/__init__.py
+++ config/__init__.py
@@ -4,8 +4,8 @@ load_dotenv()
 from os import environ as env
 
 class FlaskAppConfig:
-    SERVER_NAME = env.get('CLIENT_URL')
-    SECRET_KEY = env.get('CLIENT_SECRET')
+    SERVER_NAME = env.get('APP_URL')
+    SECRET_KEY = env.get('APP_SECRET')
 
     SQLALCHEMY_DATABASE_URI = env.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
