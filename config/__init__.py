bd034b912d47faa096cda7f0b4c31c22ceb26a7d
s4turn-dev <>
2025-09-05 00:13:14 +0300

    develop: set up environment variables config

---
+++ config/__init__.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from os import environ as env
+
+class FlaskAppConfig:
+    SECRET_KEY = os.env.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.env.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
